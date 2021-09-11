002ab7ecaf23e785bfd41843a991c1d8339c3ad6
lacouth <lacouth@gmail.com>
2021-09-11 20:47:38 -0300

    add tabela_total

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
 
+def get_ingredientes():
+    df = pd.read_csv('static/data/tabela_taco.csv')
+    return df.iloc[:,0].tolist()
+
+def calcular_tabelas(ingredientes):
+    df = pd.read_csv('static/data/tabela_taco.csv',index_col=0)
+    df.pop('Restrições')
+    df = df.loc[ingredientes.keys()]
+    peso_total = 0
+    for ingrediente in ingredientes:
+        df.loc[ingrediente] = df.loc[ingrediente] * float(ingredientes[ingrediente]['liquido']) / 100
+        peso_total += float(ingredientes[ingrediente]['liquido'])
+    tabela_total = pd.DataFrame(df.sum()).to_json()
+    tabela_proporcional = pd.DataFrame(df.sum() * 100 / peso_total).to_json()
+    return {'tabela_total':tabela_total,'tabela_proporcional':tabela_proporcional}
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', ingredientes=get_ingredientes())
+
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    dados = request.get_json()
+    return calcular_tabelas(dados)
---
+++ static/js/index.js
@@ -0,0 +1,62 @@
+let btn_adicionar = document.getElementById('btn_adicionar');
+let btn_calcular = document.getElementById('btn_calcular');
+let input_alimento = document.getElementById('alimento');
+let input_peso_bruto = document.getElementById('peso_bruto');
+let input_peso_liquido = document.getElementById('peso_liquido');
+let table = document.getElementById('table');
+let tbody = table.getElementsByTagName('tbody')[0];
+
+let tabela_total = document.getElementById('tabela_total');
+// let tbody_total = tabela_total.getElementsByTagName('tbody')[0];
+let tbody_total = document.getElementById('tbody_total');
+
+let cont = 0;
+let lista = new Object();
+
+btn_adicionar.addEventListener('click',()=>{
+    cont++; 
+
+    tbody.insertAdjacentHTML('beforeEnd', 
+    `<tr>
+        <th scope="row">${cont}</th>
+        <td>${input_alimento.value}</td>
+        <td>${input_peso_bruto.value}</td>
+        <td>${input_peso_liquido.value}</td>
+    </tr>`);
+
+    lista[input_alimento.value] = {}
+    lista[input_alimento.value]["bruto"] = input_peso_bruto.value;
+    lista[input_alimento.value]["liquido"] = input_peso_liquido.value;
+    
+    input_peso_bruto.value = "";
+    input_peso_liquido.value = "";
+    input_alimento.value = "";
+
+});
+
+btn_calcular.addEventListener('click',()=>{
+    fetch('/calcular',{
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(lista)
+    })
+    .then(res=>{
+        return res.json()
+    })
+    .then(data=>{
+        let data_json = JSON.parse(data['tabela_total'])[0] 
+        atualizar_tabela_total(data_json);
+    });
+});
+
+function atualizar_tabela_total(dados){
+    let tabela = ""
+    for(let d in dados){
+        tabela += `<tr><th scope="row">${d}</th><td>${dados[d]}</td></tr>\n`
+    }
+    console.log(tabela);
+    tbody_total.insertAdjacentHTML('beforeend',tabela);
+}
