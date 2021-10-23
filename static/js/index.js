900728cb28750f424c5f91330d2037ad54fc0f4a
lacouth <lacouth@gmail.com>
2021-10-23 13:47:46 -0300

    adiciona botão para remover ingrediente na tabela

--- static/js/index.js
+++ static/js/index.js
@@ -31,8 +31,7 @@ fetch('/get_restricoes',{
 })
 
 btn_adicionar.addEventListener('click',()=>{
-    cont++; 
-
+    
     lista[input_alimento.value] = {}
     lista[input_alimento.value]["bruto"] = input_peso_bruto.value;
     lista[input_alimento.value]["liquido"] = input_peso_liquido.value;
@@ -40,8 +39,7 @@ btn_adicionar.addEventListener('click',()=>{
     total_bruto += Number(input_peso_bruto.value)
     total_liquido += Number(input_peso_liquido.value)
 
-    td_total_bruto.innerText = total_bruto
-    td_total_liquido.innerText = total_liquido
+    atualizar_totais()    
 
     let classe_restricao = ""
     switch(restricoes[input_alimento.value]){
@@ -57,45 +55,84 @@ btn_adicionar.addEventListener('click',()=>{
     }
 
     tbody.insertAdjacentHTML('beforeEnd', 
-    `<tr class = ${classe_restricao} >
-        <th scope="row">${cont}</th>
-        <td>${input_alimento.value}</td>
-        <td>${input_peso_bruto.value}</td>
-        <td>${input_peso_liquido.value}</td>
+    `<tr id = "row_${cont}" class = ${classe_restricao} >
+        <th scope="row">${cont+1}</th>
+        <td id="alimento_${cont}">${input_alimento.value}</td>
+        <td id="p_bruto_${cont}">${input_peso_bruto.value}</td>
+        <td id="p_liquido_${cont}">${input_peso_liquido.value}</td>
+        <td onClick = "deleteRow(${cont})"> <i data-feather="trash-2"> </i> </td>
     </tr>`);
+    feather.replace()
     
     input_peso_bruto.value = "";
     input_peso_liquido.value = "";
     input_alimento.value = "";
+    cont++;
+
+    calcular_tabelas()
 
 });
 
-btn_calcular.addEventListener('click',()=>{
-    fetch('/calcular',{
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(lista)
-    })
-    .then(res=>{
-        return res.json()
-    })
-    .then(data=>{
-        let data_json_proporcional = JSON.parse(data['tabela_proporcional'])[0] 
-        let data_json_total = JSON.parse(data['tabela_total'])[0]
-        atualizar_tabela(tbody_proporcional, data_json_proporcional)
-        atualizar_tabela(tbody_total, data_json_total)
-        
-    });
-});
+function calcular_tabelas(){
+    if(cont > 0){
+        fetch('/calcular',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(lista)
+        })
+        .then(res=>{
+            return res.json()
+        })
+        .then(data=>{
+            let data_json_proporcional = JSON.parse(data['tabela_proporcional'])[0] 
+            let data_json_total = JSON.parse(data['tabela_total'])[0]
+            atualizar_tabela(tbody_proporcional, data_json_proporcional)
+            atualizar_tabela(tbody_total, data_json_total)
+            
+        });
+    }else{
+        tabela_total.innerHTML = ""
+        tabela_proporcional.innerHTML = ""
+    }
+}
+
+btn_calcular.addEventListener('click',()=>{ calcular_tabelas() });
 
 function atualizar_tabela(tabelaHTML, dados){
     let tabela = ""
+    tabelaHTML.innerHTML = tabela
     for(let d in dados){
         tabela += `<tr><th scope="row">${d}</th><td>${Number(dados[d]).toFixed(2)}</td></tr>\n`
     }
     tabelaHTML.insertAdjacentHTML('beforeend',tabela);
+}
+
+function deleteRow(pos,alimento){
+    cont--
+
+    let alimento_td = document.getElementById("alimento_"+pos)
+    alimento = alimento_td.innerText
+    delete lista[alimento]
+    
+    let p_bruto = document.getElementById("p_bruto_"+pos).innerText
+    let p_liquido = document.getElementById("p_liquido_"+pos).innerText
+
+    total_bruto -= Number(p_bruto)
+    total_liquido -= Number(p_liquido)
+
+    atualizar_totais()
+
+    let row = document.getElementById("row_"+pos)
+    row.remove()
+    calcular_tabelas()    
+
+}
+
+function atualizar_totais(){
+    td_total_bruto.innerText = total_bruto
+    td_total_liquido.innerText = total_liquido    
 }
 
